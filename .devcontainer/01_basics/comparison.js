72a98c9aa94cc5bb29cd30e01a0bd68cfb82a89d
aashi singh <143220800+aashi200@users.noreply.github.com>
2025-12-11 08:08:47 +0000

    basics

--- .devcontainer/01_basics/comparison.js
+++ .devcontainer/01_basics/comparison.js
@@ -6,4 +6,11 @@
 
 //never compare two diff datatypes
 console.log("2" > 1);
-console.log("02" > 1)
+console.log("02" > 1);
+
+console.log(undefined == 0);
+console.log(undefined > 0); // for all output will be zero
+console.log(undefined < 0);
+
+//=== strict check
+console.log("2"===2)// gives error as it first compares the datatypes conversion doesnt occur
