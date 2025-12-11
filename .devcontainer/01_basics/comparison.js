58146c10ff59fb0554e2bb066f53814e2e5815f8
aashi singh <143220800+aashi200@users.noreply.github.com>
2025-12-11 07:56:44 +0000

    basics

---
+++ .devcontainer/01_basics/comparison.js
@@ -0,0 +1,9 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//onsole.log(2 != 1);
+
+//never compare two diff datatypes
+console.log("2" > 1);
+console.log("02" > 1)
---
+++ .devcontainer/01_basics/conversiontype.js
@@ -0,0 +1,28 @@
+let score = "34aa"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+//true =1; false =0;
+
+let islog = 1
+let boolean = Boolean(islog);
+console.log(boolean);
+
+let value = 3
+let negValue = -value //basically gives a negative value
+// console.log(negValue);
+console. log(2-2);
+console.log(2+2);
+
+let str1 = "Hello"
+let str2 = " Hello"
+let str3 = str1 + str2
+console.log(str3); // gives Hello Hello
+
